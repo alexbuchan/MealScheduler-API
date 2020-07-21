c548851269299035cab3c3f3a9de3b3bedf608e9
alexbuchan <alex.buchan@telefonica.net>
2020-07-21 11:56:51 +0200

    Add unit tests and factorybot for MeasureUnit model

--- app/models/measure_unit.rb
+++ app/models/measure_unit.rb
@@ -1,3 +1,8 @@
+# frozen_string_literal: true
+
+# MeasureUnit
 class MeasureUnit < ApplicationRecord
   has_and_belongs_to_many :measure_systems
+
+  validates :name, presence: true
 end
---
+++ spec/factories/measure_unit.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :measure_unit do
+    name { "gram" }
+  end
+end
